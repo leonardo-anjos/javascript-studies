407bd90f2c31f981c515176efb914e925a7d79d4
Leonardo Anjos <leodrums11@gmail.com>
2018-03-07 13:16:50 -0300

    studies node.js on w3school

---
+++ nodejs-tutorial/myFirst.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var date = require('./modules');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+}).listen(8080);
