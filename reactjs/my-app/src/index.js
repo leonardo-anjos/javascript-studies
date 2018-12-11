91887ef0b560c3a2bbe092f9195eeb9e2397dca5
leonardo-anjos <leodrums11@gmail.com>
2018-12-11 15:18:51 -0300

    [master] my first app

--- reactjs/my-app/src/index.js
+++ reactjs/my-app/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Input, Button } from 'antd';
 
 const App = () => {
   return(
     <div>
       <h1>Hi! welcome...</h1>
       <h4>this is my first app in reactjs</h4>
+    
+      <div>
+        <Input placeholder="Basic usage" />
+        <Button type="primary">Primary</Button>
+      </div>
+      
     </div>
   );
 };
