2367a5ec763dd2b2c31eaaf6554b19350204e2cd
leonardo-anjos <leodrums11@gmail.com>
2018-12-18 17:42:05 -0300

    index

--- reactjs/seasons/src/index.js
+++ reactjs/seasons/src/index.js
@@ -1,37 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+import './SeasonDisplay.css';
 
 class App extends React.Component {
   
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      lat: null,
-      long: null
-    }
-    
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ 
-          lat: position.coords.latitude,
-          long: position.coords.longitude
-        });
-      }
-    );
+  state = {
+    lat: null,
+    long: null,
+    errorMessage: ''
   }
   
-  render() {
+  render() {    
     return(
-      <div>
-        <h2>MySeasons Locator</h2>
-        <p>Latitude: { this.state.lat }</p>
-        <p>Longitude: { this.state.long }</p>
+      <div className="border red">
+        {this.renderContent()}
       </div>
     );
-
   }
+
+  renderContent() {
+    if(this.state.errorMessage && !this.state.lat && !this.state.long) {
+      return(
+        <div>
+          <p>Error: { this.state.errorMessage }</p>
+        </div>
+      )  
+    }
+
+    if(!this.state.errorMessage && this.state.lat && this.state.long) {
+      return(
+        <SeasonDisplay lat={this.state.lat} long={this.state.long}/>
+      );
+    }
+    
+    return(
+      <Spinner message="Please accept location request"/>
+    );
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({lat: position.coords.latitude, long: position.coords.longitude}),
+      err => {this.setState({ err: err.message })}
+    );
+  };
+
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
