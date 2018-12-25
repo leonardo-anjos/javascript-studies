b8445c530cc7fc54dc32ec06cdf939a735aaa4dd
Leonardo Anjos <leodrums11@gmail.com>
2018-12-25 17:01:21 -0300

    finish app v1.0

--- reactjs/pics-v1/src/components/App.js
+++ reactjs/pics-v1/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import unsplash from '../api/unsplhash';
 import SearchBar from "./SearchBar";
 import ImageList from './ImageList';
+import '../style/App.css'
 
 export default class App extends React.Component {
 	
@@ -10,22 +11,16 @@ export default class App extends React.Component {
 	}
 	
 	onSearchSubmit = async (term) => {
-		console.log(term);
 		const response = await unsplash.get('/search/photos', {
 			params: { query: term }
 		});
-
-		console.log(this); 
 		this.setState({ images: response.data.results })
-		console.log(response.data.results);
-	
 	}
 
 	render() {
 		return(
 			<div className="ui container" style={{marginTop: '30px'}}>
 				<SearchBar onSubmit={this.onSearchSubmit}/>
-				Found: {this.state.images.length} images
 				<ImageList images={this.state.images}/>
 			</div>
 		);
--- reactjs/pics-v1/src/components/ImageList.js
+++ reactjs/pics-v1/src/components/ImageList.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 const ImageList = props => {
 	
-	const images = props.images.map(image => {
-		return <img src={image.urls.regular} />
+	const images = props.images.map(({description, id, urls}) => {
+		return(
+			<div key={id}>
+				<img alt={description} src={urls.regular} />
+			</div>
+		); 
 	});
 
 	return <div>{images}</div>
+
 }
 
 export default ImageList;
