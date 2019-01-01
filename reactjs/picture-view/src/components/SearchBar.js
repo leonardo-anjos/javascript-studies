95dcc2d1091881a2c5d736fa1052bb43dd55641b
Leonardo Anjos <leodrums11@gmail.com>
2019-01-01 18:45:18 -0300

    finish app reactjs

---
+++ reactjs/picture-view/src/components/SearchBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default class SearchBar extends React.Component {
+	
+	state = {
+		term: ''
+	}
+
+	render() {
+		return(
+			<div className="ui segment">
+				<form 
+					className="ui form"
+					onSubmit={this.onFormSubmit}>
+					<div className="field">
+						<label>Image Search</label>
+						<input 
+							type="text" 
+							onChange={(e) => this.setState({ term: e.target.value})}
+							onClick={this.onInputClick}
+							value={this.state.term}
+						/>
+					</div>
+				</form>
+			</div>
+		);
+	}
+
+	onFormSubmit = event => {
+		event.preventDefault();
+		this.props.onSubmit(this.state.term);
+	};
+	
+	onInputClick() {
+		console.log('clicked on form...');
+	};
+
+}
