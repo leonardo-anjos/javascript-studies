103fcd2579b0182f4304b1915937fd7f732cf225
Leonardo Anjos <leodrums11@gmail.com>
2019-01-03 11:09:32 -0300

    hands-on redux

--- reactjs/redux/insurance-company/InsuranceCompany.js
+++ reactjs/redux/insurance-company/InsuranceCompany.js
@@ -31,3 +31,36 @@ const deletePolicy = (name) => {
     }
   };
 };
+
+// REDUCERS
+// departaments
+const claimsHistory = (oldListOfClaims = [], action) => {
+  if (action.type == 'CREATE_CLAIM') {
+    // we care abour action (FORM)
+    // oldListOfClaims.push(action.payload); modify array with new values
+    return [...oldListOfClaims, action.payload]; // create a new array appending new values
+  }
+  // we don't care the action (form)
+  return oldListOfClaims;
+}
+
+const accounting = (bagOfMoney = 100, action) => {
+  if (action.type === 'CREATE_CLAIM') {
+    return bagOfMoney - action.payload.amountOfMoneyToCollect;
+  } else if(action.type === 'CREATE_POLICY') {
+    return bagOfMoney + action.payload.amountOfMoneyToCollect;
+  }
+  return bagOfMoney;
+}
+
+const polices = (listOfPolices = [], actions) => {
+  if (action.type === 'CREATE_POLICE') {
+    return [...listOfPolices, action.payload.name];
+  } else if(action.type === 'DELETE_POLICE') {
+    return listOfPolices.filter(name => name != action.payload.name);
+  }
+  return listOfPolices;
+}
+
+// obs: the firsth objective is get some data of some action and after modify and remove the data existing with base 
+// on the load of a action 
