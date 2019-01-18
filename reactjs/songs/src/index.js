632beddb55a2970abbff4544121f6eff0a5aac26
Leonardo Anjos <leodrums11@gmail.com>
2019-01-17 23:37:54 -0300

    super components and component to list and load data from redux

--- reactjs/songs/src/index.js
+++ reactjs/songs/src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
+import reducer from './providers/reducer';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={createStore(reducer)}>
+    <App />
+  </Provider>
+  , document.querySelector('#root')
+);
