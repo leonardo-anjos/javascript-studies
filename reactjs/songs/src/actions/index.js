816966ea41f4c99190b688592ca940edc52fa9dd
Leonardo Anjos <leodrums11@gmail.com>
2019-01-22 00:01:19 -0300

    finish application: changes - songs list OK - songs details list OK - style css OK

--- reactjs/songs/src/actions/index.js
+++ reactjs/songs/src/actions/index.js
@@ -6,5 +6,3 @@ export const selectSong = song => {
     payload: song
   };
 };
-
-  
--- reactjs/songs/src/reducers/index.js
+++ reactjs/songs/src/reducers/index.js
@@ -1,21 +1,22 @@
 import { combineReducers } from 'redux';
 
-// song list
 const songsReducer = () => {
-  // data array songs 
   return [
-    { title: 'What a beaultiful name', duration: '8:35'},
-    { title: 'Broken vessels', duration: '5:50'},
-    { title: 'Oceans', duration: '6:12'},
-    { title: 'Good good father', duration: '4:05'}
-  ]
+    { title: 'Oceans', band: 'Hillsong United', duration: '8:15' },
+    { title: 'Scandal of Grace', band: 'Hillsong United', duration: '3:59' },
+    { title: 'Endless Praise', band: 'Planetshakers', duration: '4:02' },
+    { title: 'ALL ABOUT YOU', band: 'Planetshakers', duration: '7:23' },
+    { title: 'Hosanna', band: 'Hillsong United', duration: '5:00' },
+    { title: 'Intentional', band: 'Travis Greene', duration: '4:05' }, 
+    { title: 'Heart Of Stone', band: 'Britt Nicole', duration: '6:16' }
+  ];
 };
 
-// select song
 const selectedSongReducer = (selectedSong = null, action) => {
-  if(action.type === 'SONG_SELECTED') {
+  if (action.type === 'SONG_SELECTED') {
     return action.payload;
   }
+
   return selectedSong;
 };
 
