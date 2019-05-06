cad0d3cdaf67b8372dc2c20bc8eea14d77eb1623
Leonardo Anjos <leodrums11@gmail.com>
2019-05-06 13:51:36 -0300

    refactor root files src/

--- reactjs/streams/client/src/index.js
+++ reactjs/streams/client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import App from './App';
 import reducer from './providers/reducers';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
