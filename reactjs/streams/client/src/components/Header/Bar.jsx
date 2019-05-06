0cd884e2b92a44fddf562d5b58d2674cbfc3a793
Leonardo Anjos <leodrums11@gmail.com>
2019-05-06 13:53:07 -0300

    bar head

--- reactjs/streams/client/src/components/Header/Bar.jsx
+++ reactjs/streams/client/src/components/Header/Bar.jsx
@@ -1,14 +1,17 @@
 import React from  'react';
 import { Link } from 'react-router-dom';
 import GoogleAuth from '../../services/GoogleAuth';
+import './Bar.css';
 
 const HeaderBar = () => {
   return(
-    <div className="ui secundary  pointing menu">
-      <Link to="/" className="item">Stremay</Link>
-      <div className="right menu">
-        <Link to="/" className="item">All Streams</Link>
-        <GoogleAuth />
+    <div className="main-header">
+      <div className="ui secundary  pointing menu">
+        <Link to="/" className="item">Stremay</Link>
+        <div className="right menu">
+          <Link to="/" className="item">All Streams</Link>
+          <GoogleAuth />
+        </div>
       </div>
     </div>
   );
