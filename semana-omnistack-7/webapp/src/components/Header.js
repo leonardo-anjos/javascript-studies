8692c19fed5b86f6c01c6696e07ae80d7ee1b0e1
leonardo-anjos <leodums11@gmail.com>
2019-06-13 01:02:38 -0300

    first commit of webapp structural

---
+++ semana-omnistack-7/webapp/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div className="header-content">
+        <img src="" alt="InstRocket" />
+        <img src="" alt="Enviar Publicação" />
+      </div>
+    </header>
+  );
+}
